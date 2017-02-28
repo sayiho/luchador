7a25cfd5c42d0ac431488fcd33caf5895fc074fb
moto (・_・) <mthrok@gmail.com>
2017-02-28 12:00:25 -0800

    fixup! Add an example of graph. TODO: Clean up and refactor

--- luchador/nn/base/component.py
+++ luchador/nn/base/component.py
@@ -3,5 +3,18 @@ from __future__ import absolute_import
 
 
 class Node(object):  # pylint: disable=too-few-public-methods
-    """Make subclass retrievable with get_component method"""
-    pass
+    """Make subclass retrievable with get_node method"""
+    def get_output_tensors(self):
+        return []
+
+    def get_update_operations(self):
+        return None
+
+    def get_parameters_to_train(self):
+        return []
+
+    def get_parameters_to_serialize(self):
+        return []
+
+    def get_parameter_variables(self, name=None):
+        return []
